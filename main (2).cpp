e39776b9042258d4b06a7a6ab99ef1bac5d72f29
akmalraudhi <146154199+akmalraudhi@users.noreply.github.com>
2023-10-11 09:33:15 +0700

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+string arabicToRoman(int num) {
+    vector<pair<int, string>> romans({ {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"} });
+    string roman = "";
+    while (num > 0) {
+        for (auto &[v, r]: romans) {
+            if (num >= v) {
+                num -= v;
+                roman += r;
+                break;
+            }
+        }
+    }
+    return roman;
+}
